367ef127cdf0e08b4b92212b6e539c9a2f772e39
Carlo Martinucci <carlo.martinucci@gmail.com>
2018-11-02 16:02:00 +0100

    Updates

---
+++ precache-manifest.ff6b1b4d46bbf2d76f37d9f5c34db461.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3bf351854c66b83024e",
+    "url": "/robot-line/static/js/runtime~main.f3bf3518.js"
+  },
+  {
+    "revision": "a9ddfb9189362d1baaca",
+    "url": "/robot-line/static/js/main.a9ddfb91.chunk.js"
+  },
+  {
+    "revision": "013cee75169852e0534e",
+    "url": "/robot-line/static/js/1.013cee75.chunk.js"
+  },
+  {
+    "revision": "a9ddfb9189362d1baaca",
+    "url": "/robot-line/static/css/main.f3a00d0e.chunk.css"
+  },
+  {
+    "revision": "86408c4d4156904eca833a12ec35e6ba",
+    "url": "/robot-line/index.html"
+  }
+];
